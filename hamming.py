39b76783ef522148f2b631121c5f3af479bf333d
Anas Barakat <barakatanas34@gmail.com>
2026-06-07 02:36:27 +0300

    implementing the hamming code class and initializing the required functions.

---
+++ hamming.py
@@ -0,0 +1,21 @@
+# Hamming code class definition
+
+
+class HammingCode:
+    def __init__(self):
+        pass
+
+    def encode_block(self):
+        pass
+
+    def encode(self):
+        pass
+
+    def syndrome(self):
+        pass
+
+    def decode_block(self):
+        pass
+
+    def decode(self):
+        pass
